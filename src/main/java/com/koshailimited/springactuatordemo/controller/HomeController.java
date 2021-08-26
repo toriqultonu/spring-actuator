f6d93c058a25e0524d3b8eab9353fa72cbd7edb8
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2021-08-26 10:09:56 -0700

    actuator added

--- src/main/java/com/koshailimited/springactuatordemo/controller/HomeController.java
+++ src/main/java/com/koshailimited/springactuatordemo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.koshailimited.springactuatordemo.controller;
 
+import com.koshailimited.springactuatordemo.helper.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,9 @@ import java.util.Map;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    private Student student;
 
     @GetMapping("/get-data")
     public Map<String, String> getData(){
---
+++ src/main/java/com/koshailimited/springactuatordemo/helper/Student.java
@@ -0,0 +1,11 @@
+package com.koshailimited.springactuatordemo.helper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    public Student(){
+        System.out.println("Creating the object of student");
+    }
+}
